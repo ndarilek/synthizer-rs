4a9d623b800f2f4528826f558b75e5182ec2bf55
Nolan Darilek <nolan@thewordnerd.info>
2020-09-11 09:58:46 -0500

    Add some initial methods.
    * Add error type for wrapping Synthizer error codes.
    * Add smart pointer for `syz_Handle`.
    * Add initialize and shutdown functions, as well as context initialization.

--- synthizer-rs/src/lib.rs
+++ synthizer-rs/src/lib.rs
@@ -1,7 +1,68 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::{fmt::Display, ops::Deref, ops::DerefMut};
+
+use synthizer_sys::*;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub struct SynthizerError(syz_ErrorCode);
+
+impl Display for SynthizerError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "SynthizerError({})", self.0)
+    }
+}
+
+struct Handle(syz_Handle);
+
+impl Deref for Handle {
+    type Target = syz_Handle;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Handle {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+fn wrap(v: i32) -> Result<(), SynthizerError> {
+    if v == 0 {
+        Ok(())
+    } else {
+        Err(SynthizerError(v))
+    }
+}
+
+impl Drop for Handle {
+    fn drop(&mut self) {
+        let rv = unsafe { syz_handleFree(self.0) };
+        if rv != 0 {
+            panic!("Failed to free handle");
+        }
+    }
+}
+
+pub fn initialize() -> Result<(), SynthizerError> {
+    wrap(unsafe { syz_initialize() })
+}
+
+pub fn shutdown() -> Result<(), SynthizerError> {
+    wrap(unsafe { syz_shutdown() })
+}
+
+pub struct Context(Handle);
+
+impl Context {
+    pub fn new() -> Result<Self, SynthizerError> {
+        let mut handle = Handle(0);
+        let v = unsafe { syz_createContext(&mut *handle) };
+        if v == 0 {
+            Ok(Self(handle))
+        } else {
+            Err(SynthizerError(v))
+        }
     }
 }
